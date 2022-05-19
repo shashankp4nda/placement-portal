d77db1cb26132ecb89deb9a471f4646c2d24cfc3
Shashank Panda <shashankpanda@pandas-macbook.local>
2022-05-19 15:27:52 +0530

    featured card details

--- src/components/featured.js
+++ src/components/featured.js
@@ -5,6 +5,11 @@ function Featured() {
   return (
     <div className='featured'>
         <img className='featuredImage' src='./images/facebook.png'></img>
+        <div className='details'>
+            <p><span>ROLE:</span> Software Dev</p>
+            <p><span>SKILLS REQ:</span> react, node, express</p>
+            <p><span>COMPENSATION:</span> 24LPA</p>
+        </div>
     </div>
   )
 }
