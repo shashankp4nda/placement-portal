b5fcfe46617808adf45e945998e5be7f049a8634
Shashank Panda <shashankpanda@pandas-macbook.local>
2022-05-24 08:13:18 +0530

    Made dynamic calendar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './styles/App.css';
-import Sidebar from './components/sidebar'
-import Featured from './components/featured'
-import Card from './components/card'
-import offers from './data/offer.json'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import Cal from './Cal';
 
 function App() {
   return (
-    <Dashboard />
+    <Cal />
   );
 }
 
---
+++ src/Cal.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import Sidebar from './components/sidebar'
+import Calendar from 'react-calendar/dist/umd/Calendar';
+import 'react-calendar/dist/Calendar.css';
+import './styles/Cal.css';
+import { useState } from 'react';
+
+function Cal() {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className='content'>
+        <Calendar 
+          onChange={setDate}
+          value={date}
+        />
+        <div className='appointments'>
+          <h1>Appointments</h1>
+          <p>Please select a date to view appointments</p>
+          <h4>Here are your appointments on { date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear() }</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cal;
