b01a23fdf9bbea5c55b0e2c25ea326a0b2fef56e
Quantanis-git <cybershravan@gmail.com>
2022-05-25 10:46:54 +0530

    added help full

--- src/Help.js
+++ src/Help.js
@@ -1,15 +1,49 @@
 import React from "react";
 import Sidebar from './components/sidebar'
-import "./styles/question.css";
+import "./styles/help.css";
 
 function Help()
 {
     return(
         <div className="Help">
             <Sidebar />
-            <div className="content">
-                <h1>Help</h1>
+            <div className="content-h">
+                <h1>Help</h1>      
+                <details>
+                    <summary>Dashboard</summary>
+                    <p>Contains a concise Description of all the opportunities available. </p>
+                </details>
+                <br />
+                <br />
+                <br />
+                <details>
+                    <summary>Calendar</summary>
+                    <p>Displays a calendar containing all applied interviews and tests.</p>
+                </details>
+                <br />
+                <br />
+                <br />
+                <details>
+                    <summary>Edit Profile</summary>
+                    <p>Allows Students to edit their profiles.</p>
+                </details>
+                <br />
+                <br />
+                <br />
+                <details>
+                    <summary>Question Bank</summary>
+                    <p>This page contains every aspect of interviews and tests that students could be prepared for </p>
+                </details>
+                <br></br>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <h3>For any issues contact admin@:  xyz@ssn.edu.in</h3>
             </div>
+          
         </div>
         
     )
